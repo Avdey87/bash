05fd9308dda3f9609da215c1acebf60d1c080a56
Avdey87 <avdey87@gmail.com>
2026-03-02 19:15:58 +0300

    Create 1.5.sh

---
+++ monitoring/1.5.sh
@@ -0,0 +1,31 @@
+user@ubuntu:~$ ./script_1.4.sh
+Таблица процессов с параметрами
+===============================
+
++--------+----------------+----------------------------------------+-----------+-----------+-----------+
+| PID    | NAME           | CMDLINE                                 | STATUS    | LIMITS    | FD        |
++--------+----------------+----------------------------------------+-----------+-----------+-----------+
+| 1      | systemd        | /sbin/init splash                      | S 0KB     | 1048576   | 52 fds    |
+| 2      | kthreadd       | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 3      | rcu_gp         | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 4      | rcu_par_gp     | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 5      | slub_flushwq   | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 6      | kworker/0:0H   | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 7      | kworker/u2:0   | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 8      | mm_percpu_wq   | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 9      | rcu_tasks_rude | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 10     | rcu_tasks_trac | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 11     | ksoftirqd/0    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 12     | rcu_preempt    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 13     | migration/0    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 14     | idle_inject/0  | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 15     | cpuhp/0        | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 16     | cpuhp/1        | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 17     | migration/1    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 18     | ksoftirqd/1    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 19     | kworker/1:0    | N/A                                     | S 0KB     | N/A file  | 0 fds     |
+| 20     | kworker/1:0H   | N/A                                     | S 0KB     | N/A file  | 0 fds     |
++--------+----------------+----------------------------------------+-----------+-----------+-----------+
+
+Всего отображено процессов: 20
+Для просмотра всех процессов используйте полную версию скрипта
